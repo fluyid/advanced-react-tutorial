4a2fcfe84e93704e1d757e4820934bd41cfd6f34
Kailash Nath <kailashnathashok@gmail.com>
2024-01-30 12:00:30 +0300

    modified 04-short-circuit-overview to learn how short circuits work

--- src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,20 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+  const [text, setText] = useState("");
+  const [name, setName] = useState("susan");
+
+  const greeting = (name) => {
+    return `Hello there`;
+  };
+
+  return (
+    <div>
+      <h4>Falsy OR: {text || "hello world"}</h4>
+      <h4>Falsy AND: {text && "hello world"}</h4>
+      <h4>Truthy OR: {text || "hello world"}</h4>
+      <h4>Truthy AND: {text && "hello world"}</h4>
+    </div>
+  );
 };
 export default ShortCircuitOverview;
