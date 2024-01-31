16c30557484dcbad04c18f2ccde17864a84ee32f
Kailash Nath <kailashnathashok@gmail.com>
2024-01-31 17:08:04 +0300

    using remove event listener to prevent additional issues like overuse of memory

--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -15,13 +15,11 @@ const CleanupFunction = () => {
 
 const RandomComponent = () => {
   useEffect(() => {
-    console.log("hmm, this is interesting");
-    const intId = setInterval(() => {
-      console.log("hello from interval");
-    }, 1000);
-    return () => {
-      clearInterval(intId);
+    const someFunc = () => {
+      // some logic
     };
+    window.addEventListener("scroll", someFunc);
+    return () => window.removeEventListener("scroll", someFunc);
   }, []);
   return <h1>hello there</h1>;
 };
