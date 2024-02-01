2b064f6af7494fc95679a7cdddd512628b939abf
Kailash Nath <kailashnathashok@gmail.com>
2024-02-01 14:09:01 +0300

    made new files for named export

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import Navbar from "./tutorial/04-project-structure/starter/Navbar";
+import { Home, About } from "./tutorial/04-project-structure/starter/Pages";
 
 function App() {
   return (
     <div className="container">
-      <Navbar />
+      <Home />
+      <About />
     </div>
   );
-  s;
 }
 
 export default App;
